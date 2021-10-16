c4a44c2db9f9073c6c2b57f94872cf8d3b8c3c6a
Edward Brotz <edward.brotz@gmail.com>
2021-10-16 10:14:42 -0700

    TimeReceiver now saves the latest received time

--- webevents-sse/src/main/java/com/brotz/webevents/sse/TimeReceiver.java
+++ webevents-sse/src/main/java/com/brotz/webevents/sse/TimeReceiver.java
@@ -1,12 +1,48 @@
 package com.brotz.webevents.sse;
 
+import com.brotz.webevents.sse.dto.TimeMessage;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 @Component
 public class TimeReceiver {
+    private static final Logger logger = LoggerFactory.getLogger(TimeReceiver.class);
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    /**
+     * The latest time message received.
+     */
+    private TimeMessage timeMessage = new TimeMessage(Instant.now().toEpochMilli());
+
+    /**
+     * Saves the latest time received.
+     * @param time The received time.
+     */
     @JmsListener(destination = "topic:time")
     public void receiveTime(String time) {
-        System.out.println(String.format("Received %s", time));
+        TimeMessage timeMessage;
+
+        try {
+            timeMessage = mapper.readValue(time, TimeMessage.class);
+            logger.debug("Received time " + time.toString());
+        } catch (Exception ex) {
+            logger.error("Failed to read time message", ex);
+            return;
+        }
+
+        this.timeMessage = timeMessage;
+    }
+
+    public TimeMessage getTimeMessage() {
+        return timeMessage;
     }
 }
