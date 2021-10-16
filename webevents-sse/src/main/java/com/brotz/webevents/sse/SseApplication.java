8511647bb5ac7c82129ebdfd454e6269ecff39ea
Edward Brotz <edward.brotz@gmail.com>
2021-10-16 10:15:10 -0700

    Build a JSON demarshaller in SseApplication

--- webevents-sse/src/main/java/com/brotz/webevents/sse/SseApplication.java
+++ webevents-sse/src/main/java/com/brotz/webevents/sse/SseApplication.java
@@ -1,10 +1,18 @@
 package com.brotz.webevents.sse;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SseApplication {
+    @Bean
+    public ObjectMapper jsonDemarshaller() {
+        return new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SseApplication.class);
     }
